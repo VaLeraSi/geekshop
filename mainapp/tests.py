4ef8ceeb9d1dd0db33f72db4355728caf0fa5793
Lera <valeralera22@icloud.com>
2021-12-28 23:36:36 +0300

    lesson_16. Tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from unittest.case import TestCase
 
-# Create your tests here.
+from mainapp.models import Product
+
+
+class ProductsTestCase(TestCase):
+    def test_product_print(self):
+        product_1 = Product.objects.get(name="комфорт 1")
+        product_2 = Product.objects.get(name="комфорт 2")
+        self.assertEqual(str(product_1), "комфорт 1 (дом)")
+        self.assertEqual(str(product_2), "комфорт 2 (дом)")
+
+    def test_product_get_items(self):
+        product_1 = Product.objects.get(name="комфорт 1")
+        product_3 = Product.objects.get(name="комфорт 2")
+
+        products_as_class_method = set(product_1.get_items())
+        products = set([product_1, product_3])
+
+        self.assertIsNotNone(products_as_class_method.intersection(products))
